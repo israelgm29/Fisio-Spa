bc45b549052fbe88ece8ffbb107ad43ae7030f38
develop <develop@develop-Inspiron-3420>
2021-03-30 12:06:10 -0500

    nuevo usuario casi completo falta insertar a la base

--- rehabilitacion/src/main/java/com/boot/controller/FrmnuevoPacienteController.java
+++ rehabilitacion/src/main/java/com/boot/controller/FrmnuevoPacienteController.java
@@ -1,36 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.boot.controller;
 
+import com.boot.models.HistoriaClinica;
+import com.github.sarxos.webcam.Webcam;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
-import com.sun.javafx.tk.FileChooserType;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXComboBox;
+import io.github.palexdev.materialfx.controls.MFXDatePicker;
+import java.awt.Dimension;
 import java.io.File;
 import java.io.IOException;
+import static java.lang.Thread.sleep;
 import java.net.URL;
 import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
 import javafx.scene.control.Spinner;
 import javafx.scene.image.Image;
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.Circle;
+import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javax.imageio.ImageIO;
 import org.apache.commons.io.FilenameUtils;
 import org.kordamp.ikonli.javafx.FontIcon;
 
@@ -54,7 +56,7 @@ public class FrmnuevoPacienteController implements Initializable {
     @FXML
     private JFXTextField jtxtDireccion;
     @FXML
-    private ComboBox<String> cmbGenero;
+    private MFXComboBox<String> cmbGenero;
     @FXML
     private JFXTextField jtxtCorreo;
     @FXML
@@ -64,85 +66,86 @@ public class FrmnuevoPacienteController implements Initializable {
     @FXML
     private JFXTextField JtxtPeso;
     @FXML
-    private DatePicker dtpickFechanac;
+    private MFXDatePicker dtpickFechanac;
     @FXML
     private JFXTextField jtxtTelefono;
     @FXML
-    private Circle imgPerfil;
+    private ImageView imgPerfil;
     @FXML
     private JFXButton btnTomar;
     @FXML
     private JFXButton btnGuardar;
+    @FXML
+    private MFXButton onCamera;
 
-    private Path to;
-    private Path from;
-    private File selectedFile;
+    public static Webcam webcam; // For taking pictures
+    public static boolean isCapture = false; // For stop & resume thread of camera
+    private String pathFoto = null;
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         cmbGenero.getItems().add("Masculino");
         cmbGenero.getItems().add("Femenino");
         cmbGenero.getItems().add("LGBTI");
         cmbGenero.getItems().add("Otro");
+
         // TODO
-
-    }
-
-    @FXML
-    private void TakeFoto(ActionEvent event) {
     }
 
     @FXML
     private void ChargeFoto(ActionEvent event) {
         Stage open = new Stage();
-        String destino = "src/main/resources/imagesPacientes/";
+        String destino = new String(System.getProperty("user.home").toString() + "/Documentos/imagesPacientes/");
 
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Buscar Imagen");
+        File recordsDir = new File(System.getProperty("user.home"), "Documentos/imagesPacientes");
+        if (!recordsDir.exists()) {
+            recordsDir.mkdirs();
+        }
 
         // Agregar filtros para facilitar la busqueda
         fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("All Images", "*.*"),
                 new FileChooser.ExtensionFilter("JPG", "*.jpg"),
                 new FileChooser.ExtensionFilter("PNG", "*.png")
         );
         // Obtener la imagen seleccionada
         File imgFile = fileChooser.showOpenDialog(open);
-        String formato = "jpg";
-        // Mostar la imagen
+
+        try {
+            String ext1 = FilenameUtils.getExtension(imgFile.getCanonicalPath());
+
+            if (ext1.compareTo("jpg") != 0 && ext1.compareTo("png") != 0) {
+                Alert error = new Alert(Alert.AlertType.ERROR);
+                error.setTitle("Error");
+                error.setHeaderText("Tipo de archivo no valido");
+                error.setContentText("Seleccione archivos que sean solo imagenes o con extencion .jpg, .");
+                error.showAndWait();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(FrmnuevoPacienteController.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
         if (imgFile != null) {
             try {
-                String ext1 = FilenameUtils.getExtension(imgFile.getCanonicalPath());
+                Image image = new Image("file:" + imgFile.getAbsolutePath());
+                imgPerfil.setImage(image);
 
-                if (ext1.compareTo(formato) != 0) {
-                    Alert error = new Alert(Alert.AlertType.ERROR);
-                    error.setTitle("Error");
-                    error.setHeaderText("Tipo de archivo no valido");
-                    error.setContentText("Seleccione archivos que sean solo imagenes o con extencion .jpg, .");
-                    error.showAndWait();
-
-                } else {
-                    Image image = new Image("file:" + imgFile.getCanonicalPath());
-                    imgPerfil.setFill(new ImagePattern(image));
-
-                    Path from = Paths.get(imgFile.toURI());
-                    Path to = Paths.get(destino + imgFile.getName());
-                    CopyOption[] options = new CopyOption[]{
-                        StandardCopyOption.REPLACE_EXISTING,
-                        StandardCopyOption.COPY_ATTRIBUTES
-                    };
-                    Files.copy(from, to, options);
-                }
-//                
-//                System.out.println(from);
+                Path from = Paths.get(imgFile.toURI());
+                System.out.println(destino);
+                Path to = Paths.get(destino + imgFile.getName());
+                System.out.println(to);
+                CopyOption[] options = new CopyOption[]{
+                    StandardCopyOption.REPLACE_EXISTING,
+                    StandardCopyOption.COPY_ATTRIBUTES
+                };
+                Files.copy(from, to, options);
+                pathFoto = "file:" + to.toString();
 
             } catch (IOException ex) {
                 Logger.getLogger(FrmnuevoPacienteController.class.getName()).log(Level.SEVERE, null, ex);
-
             }
+
         } else {
             Alert error = new Alert(Alert.AlertType.ERROR);
             error.setTitle("Error");
@@ -154,6 +157,66 @@ public class FrmnuevoPacienteController implements Initializable {
 
     @FXML
     private void Guardad(ActionEvent event) {
+        System.out.println(pathFoto);
+
+    }
+
+    @FXML
+    void IniciarCamara(ActionEvent event) {
+        /* Init camera */
+        webcam = Webcam.getDefault();
+        if (webcam == null) {
+            System.out.println("Camera not found !");
+            System.exit(-1);
+        }
+        webcam.setViewSize(new Dimension(640, 480));
+        webcam.open();
+
+        new VideoTacker().start();
+    }
+
+    @FXML
+    private void tomarFoto(ActionEvent event) {
+        isCapture = true;
+        Stage open = new Stage();
+        FileChooser archivo = new FileChooser();
+        File recordsDir = new File(System.getProperty("user.home"), "Documentos/imagesPacientes");
+        if (!recordsDir.exists()) {
+            recordsDir.mkdirs();
+        }
+        archivo.setInitialDirectory(recordsDir);
+        File file = archivo.showSaveDialog(open);
+
+        if (file != null)
+            try { // Save picture with .png extension
+            ImageIO.write(SwingFXUtils.fromFXImage(imgPerfil.getImage(), null), "PNG", file);
+            String dataPath = new String(file.getAbsolutePath());
+            System.out.println(dataPath);
+            new VideoTacker().stop();
+
+        } catch (IOException ex) {
+            ex.printStackTrace(); // Can't save picture
+
+        }
+
+    }
+
+
+    class VideoTacker extends Thread {
+
+        @Override
+        public void run() {
+            String file = "/imagesPacientes/";
+            while (!isCapture) { // For each 30 millisecond take picture and set it in image view
+                try {
+                    imgPerfil.setImage(SwingFXUtils.toFXImage(webcam.getImage(), null));
+
+                    sleep(30);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(FrmnuevoPacienteController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
     }
 
 }
