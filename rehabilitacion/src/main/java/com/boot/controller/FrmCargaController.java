df055e3b12f6d26007145c3ff033b27f3a07472b
develop <develop@develop-Inspiron-3420>
2021-03-30 20:25:39 -0500

    implementacion de metodos separados MVC login desde el modelo

--- rehabilitacion/src/main/java/com/boot/controller/FrmCargaController.java
+++ rehabilitacion/src/main/java/com/boot/controller/FrmCargaController.java
@@ -1,6 +1,6 @@
 package com.boot.controller;
 
-import com.boot.dataaccess.Conexion;
+import com.boot.models.OperacionHistoriaC;
 import com.jfoenix.controls.JFXButton;
 import java.io.IOException;
 import javafx.event.ActionEvent;
@@ -8,8 +8,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import java.net.URL;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
@@ -24,7 +22,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
-import javax.swing.JOptionPane;
 
 /**
  *
@@ -47,8 +44,7 @@ public class FrmCargaController implements Initializable {
     private String nombre;
 
     private static FrmCargaController instance;
- 
-    
+
     @FXML
     private JFXButton btnRegistrer;
 
@@ -78,86 +74,69 @@ public class FrmCargaController implements Initializable {
 
     @FXML
     private void login(ActionEvent event) throws IOException {
+
         if (this.txtuser.getText().isEmpty() || this.txtpassword.getText().isEmpty()) {
             Alert message = new Alert(Alert.AlertType.ERROR);
             message.setTitle("FISIOSPA");
             message.setContentText("Debe llenar los campos");
             message.setHeaderText(null);
             message.showAndWait();
-        } else if (loginadmin() == 1) {
-
-            try {
-                nombre = this.txtuser.getText();
-                Alert message = new Alert(Alert.AlertType.INFORMATION);
-                message.setTitle("FISIO-SPA");
-                message.setContentText("Bienvenido");
-                message.setHeaderText(null);
-                message.showAndWait();
-                ((Node) (event.getSource())).getScene().getWindow().hide();
-
-                Stage home = new Stage();
-                Parent root = FXMLLoader.load(getClass().getResource("/fxml/frmPrincipal.fxml"));
-
-                Scene scene = new Scene(root);
-                home.setScene(scene);
-                home.show();
-            } catch (Exception e) {
-                Logger.getLogger(FrmCargaController.class.getName()).log(Level.SEVERE, null, e);
-            }
-
         } else {
-            Alert message = new Alert(Alert.AlertType.ERROR);
-            message.setTitle("FISIO-SPA");
-            message.setContentText("Usuario o contraseña incorrectos");
-            message.setHeaderText("¡Acceso denegado!");
-            message.showAndWait();
-            this.txtpassword.setText("");
-            this.txtpassword.setText("");
+            String us = txtuser.getText();
+            String pass = txtpassword.getText();
+            OperacionHistoriaC opHc = new OperacionHistoriaC();
+            opHc.Conectar();
+            int log = opHc.loginadmin(us, pass);
 
-        }
-    }
+            if (log == 1) {
 
-    public int loginadmin() {
-        String usuario = this.txtuser.getText();
-        String clave = this.txtpassword.getText();
-        int resultado = 0;
+                try {
+                    nombre = this.txtuser.getText();
+                    Alert message = new Alert(Alert.AlertType.INFORMATION);
+                    message.setTitle("FISIO-SPA");
+                    message.setContentText("Bienvenido");
+                    message.setHeaderText(null);
+                    message.showAndWait();
+                    ((Node) (event.getSource())).getScene().getWindow().hide();
 
-        String sql = "SELECT cedula, clave\n"
-                + "  FROM public.administrador WHERE nombre='" + usuario + "' AND clave='" + clave + "'";
-        Conexion con = new Conexion();
-        con.conectar();
-        try {
-            ResultSet rst = con.ejecutarQuery(sql);
-            if (rst.next()) {
-                resultado = 1;
+                    Stage home = new Stage();
+                    Parent root = FXMLLoader.load(getClass().getResource("/fxml/frmPrincipal.fxml"));
+
+                    Scene scene = new Scene(root);
+                    home.setScene(scene);
+                    home.show();
+                } catch (Exception e) {
+                    Logger.getLogger(FrmCargaController.class.getName()).log(Level.SEVERE, null, e);
+                }
+
+            } else {
+                Alert message = new Alert(Alert.AlertType.ERROR);
+                message.setTitle("FISIO-SPA");
+                message.setContentText("Usuario o contraseña incorrectos");
+                message.setHeaderText("¡Acceso denegado!");
+                message.showAndWait();
+                this.txtpassword.setText("");
+                this.txtpassword.setText("");
             }
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, ex, "Error de conexión", JOptionPane.ERROR_MESSAGE);
-        } finally {
-            con.desconectar();
-            con.desconectar();
-
         }
-        return resultado;
+
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-      
 
     }
-
 
     @FXML
     private void SingUp(ActionEvent event) {
         try {
             Stage signup = new Stage();
             Parent root = FXMLLoader.load(getClass().getResource("/fxml/frmRegistrar.fxml"));
-            
+
             Scene scene = new Scene(root);
             signup.setScene(scene);
             signup.show();
-           
+
         } catch (IOException ex) {
             Logger.getLogger(FrmCargaController.class.getName()).log(Level.SEVERE, null, ex);
         }
